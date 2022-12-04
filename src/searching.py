38a8327f20a564f89253fb47ae7c7f8436b46ca3
ruthika28 <Ruthika28>
2022-12-04 00:24:59 -0500

    chit chat data indexer

--- src/searching.py
+++ src/searching.py
@@ -3,7 +3,15 @@ import urllib.request
 import urllib
 
 def get_search_results_from_chitchat(query):
-    pass
+    q='%20'.join(query.split(' '))
+    base_url='http://35.226.106.255:8983/solr/Chit_Chat_Indexer/select?indent=true&q.op=OR&q=preprocessed_question%3A%22'+q+'%22~5'
+    response = urllib.request.urlopen(base_url)
+    docs = json.load(response)['response']['docs']
+    if len(docs)==0:
+        base_url='http://35.226.106.255:8983/solr/Chit_Chat_Indexer/select?indent=true&q.op=OR&q=preprocessed_question%3A%22'+q+'%22~5'
+        response = urllib.request.urlopen(base_url)
+        docs = json.load(response)['response']['docs']
+    return str(docs[0]['preprocessed_answer'][0])
 
 def get_search_results_from_topic(query, topic):
     q='%20'.join(query.split(' '))
